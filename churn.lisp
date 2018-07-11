d894aaac105bdbff026c1e6bd15fc54ba6c69f50
sisto <sisto@sd.dk>
2018-07-11 22:13:37 +0200

    Example of not reading ','-formatted decimals

---
+++ churn.lisp
@@ -0,0 +1,42 @@
+(setf *read-default-float-format* 'double-float)
+(ql:quickload :clml) ;; Go to quicklisp/local-projects and clone https://github.com/mmaul/clml
+
+(defun setup-dataset (path)
+  (clml.hjs.read-data:read-data-from-file
+   (clml.utility.data:fetch path)
+   :type :csv
+   ;; This is good enough for now
+   :csv-type-spec '(integer
+                    integer
+                    double-float
+                    double-float
+                    double-float
+                    double-float
+                    integer
+                    string
+                    string
+                    string
+                    integer
+                    double-float
+                    integer
+                    double-float
+                    integer
+                    double-float
+                    integer
+                    double-float
+                    nil
+                    nil
+                    nil)
+   :csv-delimiter #\;))
+
+;; Original dataset
+(defvar churn-data (setup-dataset "./Churn.csv"))
+
+;; Manually edited to replace , with .
+(defvar churn-data-point (setup-dataset "./Churn-dot-decimal.csv"))
+
+;; Notice how all decimals are still strings in the following expression:
+(format t "~S~%" (clml.hjs.read-data:head-points churn-data))
+
+;; While everything is alright in the following
+(format t "~S~%" (clml.hjs.read-data:head-points churn-data-point))
